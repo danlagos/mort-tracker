5911b5aaa55a96c48646bd23c2f99c664c5d05d2
danlagos <lagos.d@gmail.com>
2021-07-16 18:09:25 -0700

    initial commit.

---
+++ src/components/Contact.vue
@@ -0,0 +1,44 @@
+<template>
+  <div>
+    <ul>
+      <li>FILE</li>
+      <li>CLIENT NAME</li>
+      <li>PRE APP</li>
+      <li>DATE</li>
+      <li>REFERRAL</li>
+      <li>PHONE NUMBER</li>
+      <li>EMAIL</li>
+      <li>TYPE OF LOAN</li>
+      <li>NOTES</li>
+    </ul>
+    <ul>
+      <li>DL</li>
+      <li>David Supina</li>
+      <li>APP</li>
+      <li>7/15</li>
+      <li>PCR</li>
+      <li>619-948-2070</li>
+      <li>davidsupina@icloud.com</li>
+      <li>REFI CONV</li>
+      <li>
+        He inherited the home from husband, other people $15,000 to pay off the
+        credit cards debts, Owed the home since 1993 2 years, its in his trust 2
+        months behind on payments. $1300 home is worth 1million and home
+        renovations are doing final touches. Permits have been paid, currently
+        unemployed had a rent for 6 months. 30 year Mortgage VA behind payments
+        $50,000 credit score 705 Tell him that we are unable to move forward
+        until you have income.
+      </li>
+    </ul>
+  </div>
+</template>
+
+<style scoped>
+ul {
+  list-style-type: none;
+  display: inline-flex;
+}
+li {
+  margin: 0 10px;
+}
+</style>
---
+++ src/views/Home.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="home">
+    <Contact />
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+
+import Contact from "@/components/Contact.vue";
+
+export default {
+  name: "Home",
+  components: {
+    Contact,
+  },
+};
+</script>
