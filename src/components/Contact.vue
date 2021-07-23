b3dc43f29235fe6ab68e5f611d2750234383cdf7
danlagos <lagos.d@gmail.com>
2021-07-22 21:56:46 -0700

    refactored to use state manamagment.

--- src/components/Contact.vue
+++ src/components/Contact.vue
@@ -20,49 +20,11 @@
 </template>
 
 <script>
+import { mapState } from "vuex";
 export default {
-  data() {
-    return {
-      headers: [
-        {
-          text: " Owner",
-          align: "start",
-          sortable: false,
-          value: "owner",
-        },
-        { text: "Client", value: "client" },
-        { text: "Pre App", value: "preApp" },
-        { text: "Date", value: "date" },
-        { text: "Referal Source", value: "referalSource" },
-        { text: "Phone Number", value: "phoneNumber" },
-        { text: "Email", value: "email" },
-        { text: "Type of Loan", value: "tpyeOfLoan" },
-        { text: "Notes", value: "notes" },
-      ],
-      contacts: [
-        {
-          owner: "DL",
-          client: "David Supina",
-          preApp: "App",
-          date: "7/11",
-          referalSource: "PCR",
-          phoneNumber: "619-948-2070",
-          email: "davidsupina@icloud.com",
-          tpyeOfLoan: "Conv Refi",
-          notes: "He inherited the home from husband",
-        },
-      ],
-    };
+  computed: {
+    // in case you need a localComputed() {},
+    ...mapState(["headers", "contacts"]),
   },
 };
 </script>
-
-<style scoped>
-ul {
-  list-style-type: none;
-  display: inline-flex;
-}
-li {
-  margin: 0 10px;
-}
-</style>
